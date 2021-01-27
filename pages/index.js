2f937a07af9d715697bc66df93ecdea99c0a225f
alessandervs <sanderpant1@hotmail.com>
2021-01-27 11:20:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,22 @@
 function Home(){
-    return <div>Home</div>
+    return <div>
+                <!--Start of Tawk.to Script-->
+                <script type="text/javascript">
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/60117434a9a34e36b970f398/1et22rrkq';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+                </script>
+                <!--End of Tawk.to Script-->
+
+        </div>
 }
 
 export default Home
 
-function contact(){
-return <div>
-    
-    <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/60117434a9a34e36b970f398/1et22rrkq';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-        </script>
-        <!--End of Tawk.to Script-->
-    
-    </div>
-}
 
-export default contact
